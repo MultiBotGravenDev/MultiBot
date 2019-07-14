dc9b0909eb6c6f79bda8db6f39b81fc553ffe35a
LukaMrt <a>
2019-07-14 10:10:09 +0200

    create serializer, loader, writer

---
+++ src/main/java/fr/gravendev/multibot/json/FileLoader.java
@@ -0,0 +1,37 @@
+package fr.gravendev.multibot.json;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileLoader {
+
+    private FileLoader() {}
+
+    public static String load(File file) {
+
+        final StringBuilder text = new StringBuilder();
+
+        try {
+            file.createNewFile();
+
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                text.append(line);
+            }
+
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return text.toString();
+
+    }
+
+}
---
+++ src/main/java/fr/gravendev/multibot/json/FileWriter.java
@@ -0,0 +1,30 @@
+package fr.gravendev.multibot.json;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+
+public class FileWriter {
+
+    private FileWriter(){}
+
+    public static void writeFile(File file, String content) {
+
+        final BufferedWriter fw;
+
+        try {
+            file.createNewFile();
+
+            fw = new BufferedWriter(new java.io.FileWriter(file));
+            fw.write(content);
+            fw.flush();
+            fw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
