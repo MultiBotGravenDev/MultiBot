aee9765b9118cef9b1656f4e2561a4dcfe8bb03e
LukaMrt <a>
2019-07-15 21:27:49 +0200

    cancel event message if sender is bot

--- src/main/java/fr/gravendev/multibot/commands/commands/WelcomeMessageCommand.java
+++ src/main/java/fr/gravendev/multibot/commands/commands/WelcomeMessageCommand.java
@@ -5,17 +5,11 @@ import fr.gravendev.multibot.commands.CommandExecutor;
 import fr.gravendev.multibot.data.WelcomeMessageData;
 import fr.gravendev.multibot.database.DatabaseConnection;
 import fr.gravendev.multibot.database.dao.WelcomeMessageDAO;
-import fr.gravendev.multibot.json.FileLoader;
-import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
+import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
-import java.io.File;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -47,24 +41,13 @@ public class WelcomeMessageCommand extends CommandExecutor {
 
         try {
 
-            List<String> sentences = new ArrayList<>();
+            List<String> sentences = loadSentences();
 
-            WelcomeMessageDAO welcomeMessageDAO = new WelcomeMessageDAO(this.databaseConnection.getConnection());
-            WelcomeMessageData welcomeMessageData;
-
-            for (int i = 1; (welcomeMessageData = welcomeMessageDAO.get(i + "")) != null; ++i) {
-                sentences.add(welcomeMessageData.message);
+            for (int i = 0; i < sentences.size() - 1; i++) {
+                message.getChannel().sendMessage(sentences.get(i)).queue();
             }
 
-            for (String sentence : sentences) {
-
-                message.getChannel().sendMessage(sentence).queue(sentMessage -> {
-                    if (sentences.indexOf(sentence) == sentences.size() - 1) {
-                        sentMessage.addReaction("\u2705").queue();
-                    }
-                });
-
-            }
+            message.getChannel().sendMessage(sentences.get(sentences.size() - 1)).queue(sentMessage -> sentMessage.addReaction("\u2705").queue());
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -72,6 +55,19 @@ public class WelcomeMessageCommand extends CommandExecutor {
 
         message.delete().queue();
 
+    }
+
+    private List<String> loadSentences() throws SQLException {
+        List<String> sentences = new ArrayList<>();
+
+        WelcomeMessageDAO welcomeMessageDAO = new WelcomeMessageDAO(this.databaseConnection.getConnection());
+        WelcomeMessageData welcomeMessageData;
+
+        for (int i = 1; (welcomeMessageData = welcomeMessageDAO.get(i + "")) != null; ++i) {
+            sentences.add(welcomeMessageData.message);
+        }
+
+        return sentences;
     }
 
 }
