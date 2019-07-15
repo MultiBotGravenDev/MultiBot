1bdf873f53180c25afd480c7774afbcf22ad9017
Nolan <kiwiterra@gmail.com>
2019-07-15 15:00:48 +0200

    Add database login in configuration.json

--- src/main/java/fr/gravendev/multibot/json/Configuration.java
+++ src/main/java/fr/gravendev/multibot/json/Configuration.java
@@ -7,6 +7,7 @@ public class Configuration {
     public static final File CONFIGURATION_FILE = new File("configuration.json");
 
     private String token;
+    private String host, username, password, database;
     private char prefix;
 
     public String getToken() {
@@ -17,4 +18,19 @@ public class Configuration {
         return prefix;
     }
 
+    public String getHost() {
+        return host;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
 }
