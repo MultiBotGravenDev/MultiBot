da64cfb3a16511d28b0b317ee26e398c2461d075
LukaMrt <a>
2019-07-15 10:18:04 +0200

    change command system

--- src/main/java/fr/gravendev/multibot/commands/commands/AboutCommand.java
+++ src/main/java/fr/gravendev/multibot/commands/commands/AboutCommand.java
@@ -1,38 +1,34 @@
 package fr.gravendev.multibot.commands.commands;
 
-import fr.gravendev.multibot.commands.ChannelCommandExecutor;
 import fr.gravendev.multibot.commands.ChannelType;
+import fr.gravendev.multibot.commands.CommandExecutor;
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
 
-public class AboutCommandExecutor implements ChannelCommandExecutor {
+import java.util.Collections;
+import java.util.List;
 
-    @Override
+public class AboutCommand extends CommandExecutor {
+
     public String getCommand() {
         return "about";
     }
 
-    @Override
     public String getDescription() {
         return "Commande à propos ¯\\_(ツ)_/¯";
     }
 
-    @Override
     public ChannelType getChannelType() {
         return ChannelType.ALL;
     }
 
-    @Override
-    public void execute(Message message, String[] args) {
-
-        if (isAuthorizedChannel(message.getChannel()))
-        message.getChannel().sendMessage("Le MultiBot a été développé par les piliers de la commu.").queue();
-
+    public List<String> getAuthorizedChannelsNames() {
+        return Collections.singletonList("_commandes");
     }
 
-    @Override
-    public boolean isAuthorizedChannel(MessageChannel channel) {
-        return "_commandes".equalsIgnoreCase(channel.getName());
+    public void execute(Message message, String[] args) {
+
+        message.getChannel().sendMessage("Le MultiBot a été développé par les piliers de la commu.").queue();
+
     }
 
 }
