c958338df347fcdf0abd3747b4675a36e675c0be
Nolan <kiwiterra@gmail.com>
2019-07-16 14:53:46 +0200

    Remove unsed import in EmoteAddedListener

--- src/main/java/fr/gravendev/multibot/events/listeners/EmoteAddedListener.java
+++ src/main/java/fr/gravendev/multibot/events/listeners/EmoteAddedListener.java
@@ -5,8 +5,6 @@ import fr.gravendev.multibot.events.Listener;
 import fr.gravendev.multibot.events.listeners.emoteaddedexecutors.CandidsExecutor;
 import fr.gravendev.multibot.events.listeners.emoteaddedexecutors.ReadThisSaloonExecutor;
 import fr.gravendev.multibot.quiz.QuizManager;
-import fr.gravendev.multibot.utils.GuildUtils;
-import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 
 import java.util.Arrays;
