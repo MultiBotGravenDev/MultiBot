17ec4c494e2cfde2dae9b182dc6f82c15640fd98
Nolan <kiwiterra@gmail.com>
2019-07-17 10:35:30 +0200

    Add logs for presentation and mentionned roles

--- src/main/java/fr/gravendev/multibot/database/dao/LogsDAO.java
+++ src/main/java/fr/gravendev/multibot/database/dao/LogsDAO.java
@@ -1,25 +1,25 @@
 package fr.gravendev.multibot.database.dao;
 
-import fr.gravendev.multibot.utils.json.Serializer;
-import net.dv8tion.jda.core.entities.Message;
+import fr.gravendev.multibot.logs.MessageData;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class LogsDAO extends DAO<Message>{
+public class LogsDAO extends DAO<MessageData>{
 
     public LogsDAO(Connection connection) {
         super(connection);
     }
 
     @Override
-    public boolean save(Message message) {
+    public boolean save(MessageData message) {
         try (PreparedStatement statement = this.connection.prepareStatement(
-                "INSERT INTO logs(`discord_id`, `message`) VALUES (?, ?)")){
+                "INSERT INTO logs(`discord_id`, `message_id`, `content`, `creation`) VALUES (?, ?, ?, NOW())")){
 
-            statement.setString(1, message.getAuthor().getId());
-            statement.setString(2, new Serializer<Message>().serialize(message));
+            statement.setString(1, message.getDiscordID());
+            statement.setString(2, message.getMessage_id());
+            statement.setString(3, message.getContent());
 
             statement.executeUpdate();
 
@@ -31,12 +31,12 @@ public class LogsDAO extends DAO<Message>{
     }
 
     @Override
-    public Message get(String value) {
+    public MessageData get(String value) {
         return null;
     }
 
     @Override
-    public void delete(Message obj) {
+    public void delete(MessageData obj) {
 
     }
 }
