2aab3b40989bbfd053a1fef98ade6ff4ace87850
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-08-17 22:41:55 +0200

    Refactor Utils, fix some spelling issues

--- src/main/java/fr/gravendev/multibot/utils/Utils.java
+++ src/main/java/fr/gravendev/multibot/utils/Utils.java
@@ -25,29 +25,72 @@ public class Utils {
     }
 
     public static MessageEmbed buildEmbed(Color color, String message) {
-        return new EmbedBuilder().setColor(color).setDescription(message).build();
+        return new EmbedBuilder()
+                .setColor(color)
+                .setDescription(message)
+                .build();
     }
 
+    private static String stringifyEndDate(Date end){
+        if (end != null){
+            return getDateFormat().format(end);
+        }
+        return "Jamais";
+    }
+
+    private static String makeDescription(User user, String action, String reason){
+        String userTag = user.getAsTag();
+
+        return TextFormatter.formatLines(
+                userTag + " a été " + action + "!",
+                "Raison: " + reason
+        ).toString();
+    }
+
+    private static MessageEmbed makeEmbed(User user, String action, String reason){
+        String description = makeDescription(user, action, reason);
+
+        return new EmbedBuilder()
+                .setColor(Color.DARK_GRAY)
+                .setDescription(description)
+                .build();
+    }
+
+    private static String makeDescription(User user, String action, String reason, Date end){
+        // TODO Find a better name than beginning if possible
+        String beginning = makeDescription(user, action, reason);
+        String endDate = stringifyEndDate(end);
+
+        return TextFormatter.formatLines(
+                beginning,
+                "Fin le: " + endDate
+        ).toString();
+    }
+
+    private static MessageEmbed makeEmbed(User user, String action, String reason, Date end){
+        String description = makeDescription(user, action, reason, end);
+
+        return new EmbedBuilder()
+                .setColor(Color.DARK_GRAY)
+                .setDescription(description)
+                .build();
+    }
+
+    // TODO Put a verb at the beginning of some of those methods (get for example)
     public static MessageEmbed banEmbed(User user, String reason, Date end) {
-        return new EmbedBuilder().setColor(Color.DARK_GRAY).setDescription(user.getAsTag() + " a été bannis ! \n" +
-                "Raison: " + reason + "\n" +
-                "Fin le: " + (end != null ? Utils.getDateFormat().format(end) : "Jamais")).build();
+        return makeEmbed(user, "banni", reason, end);
     }
 
     public static MessageEmbed kickEmbed(User user, String reason) {
-        return new EmbedBuilder().setColor(Color.DARK_GRAY).setDescription(user.getAsTag() + " a été éjecté ! \n" +
-                "Raison: " + reason).build();
+        return makeEmbed(user, "éjecté", reason);
     }
 
     public static MessageEmbed muteEmbed(User user, String reason, Date end) {
-        return new EmbedBuilder().setColor(Color.DARK_GRAY).setDescription(user.getAsTag() + " a été mute ! \n" +
-                "Raison: " + reason + "\n" +
-                "Fin le: " + (end != null ? Utils.getDateFormat().format(end) : "Jamais")).build();
+        return makeEmbed(user, "rendu muet", reason, end);
     }
 
     public static MessageEmbed warnEmbed(User user, String reason) {
-        return new EmbedBuilder().setColor(Color.DARK_GRAY).setDescription(user.getAsTag() + " a été avertis ! \n" +
-                "Raison: " + reason).build();
+        return makeEmbed(user, "averti", reason);
     }
 
     private static final Map<String, Long> TIME_FORMAT = new HashMap<>();
@@ -62,6 +105,7 @@ public class Utils {
         TIME_FORMAT.put("ms", 1L);
     }
 
+    // TODO To refactor!!
     public static long parsePeriod(String parser) {
         Objects.requireNonNull(parser, "Parser cannot be nul");
 
@@ -73,9 +117,9 @@ public class Utils {
         long time = 0;
 
         for (int i = 0; i < chars.length; i++) {
-            if (Character.isDigit(chars[i]))
+            if (Character.isDigit(chars[i])){
                 timeBuilder.append(chars[i]);
-            else {
+            } else {
                 keyBuilder.append(chars[i]);
                 if (i == chars.length - 1 || Character.isDigit(chars[i + 1])) {
                     Long mul = TIME_FORMAT.get(keyBuilder.toString());
@@ -91,8 +135,9 @@ public class Utils {
             }
         }
 
-        if (keyBuilder.length() > 0 || timeBuilder.length() > 0)
+        if (keyBuilder.length() > 0 || timeBuilder.length() > 0){
             return -1;
+        }
 
         return time;
     }
