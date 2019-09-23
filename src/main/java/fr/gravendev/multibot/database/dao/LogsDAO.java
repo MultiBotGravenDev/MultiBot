cb95f8d16455de1606c9f385a99d6d5162d06be9
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-09-24 00:31:26 +0200

    Remove a variable in get from LogsDAO.java to improve verbosity

--- src/main/java/fr/gravendev/multibot/database/dao/LogsDAO.java
+++ src/main/java/fr/gravendev/multibot/database/dao/LogsDAO.java
@@ -6,7 +6,6 @@ import fr.gravendev.multibot.logs.MessageData;
 import java.sql.*;
 
 public class LogsDAO extends DAO<MessageData> {
-
     public LogsDAO(DatabaseConnection databaseConnection) {
         super(databaseConnection);
     }
@@ -19,33 +18,27 @@ public class LogsDAO extends DAO<MessageData> {
         statement.setString(1, message.getDiscordID());
         statement.setString(2, message.getMessage_id());
         statement.setString(3, message.getContent());
-
         statement.executeUpdate();
-
         return true;
     }
 
 
     @Override
     protected MessageData get(String value, Connection connection) throws SQLException {
-        MessageData messageData = null;
-
         PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM logs WHERE message_id = ?");
+
         preparedStatement.setString(1, value);
 
         ResultSet resultSet = preparedStatement.executeQuery();
 
         if (resultSet.next()) {
-
             String discord_id = resultSet.getString("discord_id");
             String message_id = resultSet.getString("message_id");
             String content = resultSet.getString("content");
             Date creation = resultSet.getDate("creation");
-            messageData = new MessageData(discord_id, message_id, content, creation.getTime());
-
+            return new MessageData(discord_id, message_id, content, creation.getTime());
         }
-
-        return messageData;
+        return null;
     }
 
     @Override
