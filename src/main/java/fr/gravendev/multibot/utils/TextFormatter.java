8cec65297f44253fa81620bf5b691ed2f397174c
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-10-07 10:57:38 +0200

    Update TextFormatter.java

--- src/main/java/fr/gravendev/multibot/utils/TextFormatter.java
+++ src/main/java/fr/gravendev/multibot/utils/TextFormatter.java
@@ -1,37 +1,35 @@
 package fr.gravendev.multibot.utils;
 
-import java.io.PrintStream;
-
 /*
     Class written by Antoine James Tournepiche
-    Cames from JASC :
+    From JASC :
         https://github.com/AntoineJT/jasc
 
     Original file :
         https://github.com/AntoineJT/jasc/blob/0b854e6d265c88ceb49f3a556f2b01a80b87007f/src/com/github/antoinejt/jasc/util/TextFormat.java
 */
 public class TextFormatter {
-    private static final PrintStream sysout = System.out;
 
     public static final class FormattedText {
         private final String formattedText;
 
-        private FormattedText(String formattedText){
+        private FormattedText(String formattedText) {
             this.formattedText = formattedText;
         }
 
-        public String toString(){
+        @Override
+        public String toString() {
             return formattedText;
         } // It's not needed to call that, implicit call will be done when you print the object (?)
 
-        public void print(){
-            sysout.print(formattedText);
+        public void print() {
+            System.out.print(formattedText);
         }
     }
 
-    public static FormattedText formatLines(String prefix, String separator, String[] lines){
+    public static FormattedText formatLines(String prefix, String separator, String[] lines) {
         StringBuilder stringBuilder = new StringBuilder();
-        for (String line : lines){
+        for (String line : lines) {
             stringBuilder
                     .append(prefix)
                     .append(line)
@@ -41,18 +39,18 @@ public class TextFormatter {
         return new FormattedText(text);
     }
 
-    public static FormattedText formatLines(String prefix, String[] lines){
+    public static FormattedText formatLines(String prefix, String[] lines) {
         return formatLines(prefix, "\n", lines);
     }
 
     // TODO Find a better name for that I think
-    public static FormattedText formatLines(String... lines){
+    public static FormattedText formatLines(String... lines) {
         return formatLines("", lines);
     }
 
-    public static void printLines(String... lines){
+    public static void printLines(String... lines) {
         FormattedText formattedLines = formatLines(lines);
-        sysout.print(formattedLines);
+        System.out.print(formattedLines);
     }
 
 }
