32b0d19732ee45aa62820e0c7bee73324815d4a4
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-10-14 02:17:37 +0200

    Make use of TextFormatter in RedescendsCommand

--- src/main/java/fr/gravendev/multibot/commands/commands/RedescendsCommand.java
+++ src/main/java/fr/gravendev/multibot/commands/commands/RedescendsCommand.java
@@ -1,8 +1,10 @@
 package fr.gravendev.multibot.commands.commands;
 
+import fr.gravendev.multibot.utils.TextFormatter;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import org.w3c.dom.Text;
 
 public class RedescendsCommand implements CommandExecutor {
 
@@ -28,10 +30,12 @@ public class RedescendsCommand implements CommandExecutor {
 
     @Override
     public void execute(Message message, String[] args) {
+        String formattedText = TextFormatter.formatLines(
+                "Il semblerait que tu surestimes tes compétences et que tu dénigres les autres : tu sembles souffrir d'un effet de Dunning-Kruger.",
+                "",
+                "Nous t'invitons donc à découvrir ce dont il s'agit : https://fr.wikipedia.org/wiki/Effet_Dunning-Kruger et à corriger cela, pour toi et pour les autres.").toString();
 
-        message.getChannel().sendMessage("Il semblerait que tu surestimes tes compétences et que tu dénigres les autres : tu sembles souffrir d'un effet de Dunning-Kruger.\n" +
-                "\nNous t'invitons donc à découvrir ce dont il s'agit : https://fr.wikipedia.org/wiki/Effet_Dunning-Kruger et à corriger cela, pour toi et pour les autres.").queue();
-
+        message.getChannel().sendMessage(formattedText).queue();
     }
 
 }
