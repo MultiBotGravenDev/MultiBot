edaa2c6c70524e59d14d038daadfeae14dac1070
Antoine James Tournepiche <antoinejt.serveur@gmail.com>
2019-10-14 03:02:21 +0200

    Refactor GuildMessageReceivedListener

--- src/main/java/fr/gravendev/multibot/logs/GuildMessageReceivedListener.java
+++ src/main/java/fr/gravendev/multibot/logs/GuildMessageReceivedListener.java
@@ -5,8 +5,10 @@ import fr.gravendev.multibot.database.dao.LogsDAO;
 import fr.gravendev.multibot.events.Listener;
 import fr.gravendev.multibot.utils.Configuration;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
@@ -32,47 +34,57 @@ public class GuildMessageReceivedListener implements Listener<GuildMessageReceiv
     @Override
     public void executeListener(GuildMessageReceivedEvent event) {
         Message message = event.getMessage();
-        MessageChannel channel = message.getChannel();
         User user = message.getAuthor();
-        if (user.isBot() || !message.getGuild().getId().equals(Configuration.GUILD.getValue())) return;
+        Guild guild = message.getGuild();
+
+        if (user.isBot()
+                || !guild.getId().equals(Configuration.GUILD.getValue())) {
+            return;
+        }
+
+        String userId = user.getId();
+        String avatarUrl = user.getAvatarUrl();
+        String userName = user.getName();
+        String jumpUrl = message.getJumpUrl();
+        MessageChannel channel = message.getChannel();
 
         if (channel.getName().startsWith("présentation-")) {
-
             MessageData messageData = new MessageData(message);
             logsDAO.save(messageData);
 
-            String logs = Configuration.LOGS.getValue();
-
-            EmbedBuilder embedBuilder = new EmbedBuilder().setColor(Color.ORANGE)
-                    .setAuthor(user.getName(), user.getAvatarUrl())
+            MessageEmbed messageEmbed = new EmbedBuilder()
+                    .setColor(Color.ORANGE)
+                    .setAuthor(userName, avatarUrl)
                     .setDescription("Projet envoyé dans <#" + channel.getId() + ">")
-                    .addField("Aller au message: ", "[Lien](" + message.getJumpUrl() + ")", false)
-                    .setFooter("User ID: " + user.getId(), user.getAvatarUrl());
+                    .addField("Aller au message: ", "[Lien](" + jumpUrl + ")", false)
+                    .setFooter("User ID: " + userId, avatarUrl).build();
 
-            TextChannel logsChannel = message.getGuild().getTextChannelById(logs);
-            if (logsChannel != null) {
-                logsChannel.sendMessage(embedBuilder.build()).queue();
-            }
+            sendMessageIfNotNull(guild, messageEmbed);
         }
 
-        List<Role> mentionedRoles = message.getMentionedRoles();
-        if (mentionedRoles.size() > 0) {
+        List<Role> mentionedRoleList = message.getMentionedRoles();
 
-            String logs = Configuration.LOGS.getValue();
-
-            EmbedBuilder embedBuilder = new EmbedBuilder().setColor(Color.MAGENTA)
-                    .setAuthor(user.getName(), user.getAvatarUrl())
+        if (mentionedRoleList.size() > 0) {
+            String mentionedRoles = mentionedRoleList.stream()
+                    .map(Role::getName)
+                    .collect(Collectors.joining(", "));
+            MessageEmbed messageEmbed = new EmbedBuilder().setColor(Color.MAGENTA)
+                    .setAuthor(userName, avatarUrl)
                     .setDescription("Rôle mentionné dans <#" + channel.getId() + ">")
-                    .addField("Rôle mentionné: ", mentionedRoles.stream().map(Role::getName).collect(Collectors.joining(", ")), false)
-                    .addField("Aller au message: ", "[Lien](" + message.getJumpUrl() + ")", false)
-                    .setFooter("User ID: " + user.getId(), user.getAvatarUrl());
+                    .addField("Rôle mentionné: ", mentionedRoles, false)
+                    .addField("Aller au message: ", "[Lien](" + jumpUrl + ")", false)
+                    .setFooter("User ID: " + userId, avatarUrl).build();
 
-            TextChannel logsChannel = message.getGuild().getTextChannelById(logs);
-            if (logsChannel != null) {
-                logsChannel.sendMessage(embedBuilder.build()).queue();
-            }
-
+            sendMessageIfNotNull(guild, messageEmbed);
         }
+    }
 
+    private void sendMessageIfNotNull(Guild guild, MessageEmbed messageEmbed) {
+        String logs = Configuration.LOGS.getValue();
+        TextChannel logsChannel = guild.getTextChannelById(logs);
+
+        if (logsChannel != null) {
+            logsChannel.sendMessage(messageEmbed).queue();
+        }
     }
 }
