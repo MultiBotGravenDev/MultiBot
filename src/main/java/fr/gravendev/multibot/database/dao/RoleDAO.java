51e19b98937957e4cb0610dadb36b16a2c0a7c33
LukaMrt <maret.luka@gmail.com>
2019-12-06 22:52:00 +0100

    add aide command

--- src/main/java/fr/gravendev/multibot/database/dao/RoleDAO.java
+++ src/main/java/fr/gravendev/multibot/database/dao/RoleDAO.java
@@ -16,29 +16,32 @@ public class RoleDAO extends DAO<RoleData> {
 
     @Override
     protected boolean save(RoleData roleData, Connection connection) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO roles VALUES(?, ?) ON DUPLICATE KEY UPDATE emote_id = ?");
+        PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO roles VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE emote_id = ?");
 
         preparedStatement.setString(1, roleData.getRoleId());
         preparedStatement.setString(2, roleData.getEmoteId());
-        preparedStatement.setString(3, roleData.getEmoteId());
+        preparedStatement.setString(3, roleData.getChannelId());
+        preparedStatement.setString(4, roleData.getEmoteId());
         preparedStatement.execute();
         return true;
     }
 
     @Override
     protected RoleData get(String value, Connection connection) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM roles WHERE role_id = ? OR emote_id = ?");
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM roles WHERE role_id = ? OR emote_id = ? OR channel_id LIKE ?");
 
         preparedStatement.setString(1, value);
         preparedStatement.setString(2, value);
+        preparedStatement.setString(3, value);
 
         ResultSet resultSet = preparedStatement.executeQuery();
 
         if (resultSet.next()) {
             String roleId = resultSet.getString("role_id");
             String emoteID = resultSet.getString("emote_id");
+            String channelId = resultSet.getString("channel_id");
 
-            return new RoleData(roleId, emoteID);
+            return new RoleData(roleId, emoteID, channelId);
         }
         return null;
     }
@@ -50,4 +53,5 @@ public class RoleDAO extends DAO<RoleData> {
         preparedStatement.setString(1, String.valueOf(roleData.getRoleId()));
         preparedStatement.execute();
     }
+
 }
--- src/main/java/fr/gravendev/multibot/database/data/RoleData.java
+++ src/main/java/fr/gravendev/multibot/database/data/RoleData.java
@@ -1,12 +1,15 @@
 package fr.gravendev.multibot.database.data;
 
 public class RoleData {
+
     private final String roleId;
     private final String emoteId;
+    private final String channelId;
 
-    public RoleData(String roleId, String emoteId) {
+    public RoleData(String roleId, String emoteId, String channelId) {
         this.roleId = roleId;
         this.emoteId = emoteId;
+        this.channelId = channelId;
     }
 
     public String getRoleId() {
@@ -16,4 +19,9 @@ public class RoleData {
     public String getEmoteId() {
         return emoteId;
     }
+
+    public String getChannelId() {
+        return channelId;
+    }
+
 }
